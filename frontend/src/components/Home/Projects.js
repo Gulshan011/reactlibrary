94d7fc8f2d95f58a1c33e5087e993671a0b81033
Gulshan011 <gulshandeep0014@gmail.com>
2023-05-26 20:51:50 +0530

    mm

---
+++ frontend/src/components/Home/Projects.js
@@ -0,0 +1,82 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../../images/eter.jpeg";
+import projImg2 from "../../images/facility.jpeg";
+import projImg3 from "../../images/fun.jpeg";
+import projImg4 from "../../images/hostel.jpeg";
+import projImg5 from "../../images/nurse.jpg";
+import projImg6 from "../../images/cafe.jpeg";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "College Library",
+      description: " Engineering Workshop ",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Facilities ",
+      description: "24 Hour Electric Supply",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Activities",
+      description: "We promote students to do co-curriculur activities",
+      imgUrl: projImg3,
+    },
+    {
+      title: "Hostel",
+      description: "Rooms are spacious, neat and clean with separate wardrobes with their individual lock key is provided to each Student",
+      imgUrl: projImg4,
+    },
+    {
+      title: "Nursing College ",
+      description: "We offer 24*7 HOSPITAL SERVICES",
+      imgUrl: projImg5,
+    },
+    {
+      title: "Cafeteria",
+      description: "We even have a cafeteria service for the students and visitors",
+      imgUrl: projImg6,
+    },
+  ];
+
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Gallery</h2>
+                <p>The Kalgidhar Trust established Eternal University for the upliftment of girl childs ;uplifting a girl is uplifting a society .Some highlights of Eternal University !!!</p>
+              
+                 
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    
+                    
+           
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+
+    </section>
+  )
+}
--- frontend/src/components/user/ChatBotWidget.js
+++ frontend/src/components/user/ChatBotWidget.js
@@ -1,19 +1,51 @@
 
-import React from 'react';
+// import { co } from '@fullcalendar/core/internal-common';
+// import React from 'react';
+// import Sidebar from "./Sidebar";
 
 
-
-function ChatBotWidget() {
+// function ChatBotWidget() {
  
 
-  return (
-    <div className='home-page'>
-      <iframe
-        src="https://www.botlibre.com/chat?&id=47626293&embedded=true&chatLog=true&facebookLogin=false&application=8782902681884078890&user=kaur0123&token=658262256558553076&password=5F7EBADD1219F47ECC4B051AF6B32599&bubble=true&menubar=true&chooseLanguage=true&sendImage=true&background=%23fff&prompt=You+say&send=&css=&language=en"
-        title="Chat"
-        style={{ width: '80%', height: '800px', border: 'none' ,borderRadius:"20px",margin:"20px"}}
-      />
+//   return (
+//     <div>
+//     <Sidebar/>
+//     <div className='home-page'>
+ 
+//     <iframe style={{borderRadius:"20px"}}allow="microphone 'src'" src="https://www.botlibre.com/chat?&id=47626293&embedded=true&chatLog=true&facebookLogin=false&application=8782902681884078890&bubble=true&menubar=true&chooseLanguage=true&sendImage=true&background=%23fff&prompt=You+say&send=&css=https://www.botlibre.com/css/social_chat.css&language=en" width="700" height="550" frameborder="0" scrolling="auto"> <style>
+
+//     #menubar {
+//       color: #f1f1f1;
+//       color: #333;
+//       font-size: 16px;
+//       padding: 10px;
+//     }
+//   </style></iframe>
      
+//     </div></div>
+//   );
+// }
+
+// export default ChatBotWidget;
+import React from 'react';
+import Sidebar from "./Sidebar";
+
+function ChatBotWidget() {
+  return (
+    <div>
+      <Sidebar />
+      <div className='home-page'>
+        <iframe
+          style={{ borderRadius: "30px" }}
+          allow="microphone 'src'"
+          src="https://www.botlibre.com/chat?&id=47626293&embedded=true&chatLog=true&facebookLogin=false&application=8782902681884078890&bubble=true&menubar=true&chooseLanguage=true&sendImage=true&background=%23fff&prompt=You+say&send=&css=https://www.botlibre.com/css/social_chat.css&language=en"
+          width="800"
+          height="650"
+          frameborder="5px solid black"
+          scrolling="auto"
+         
+        ></iframe>
+      </div>
     </div>
   );
 }
