f3913659a3b6b194ec1a841e1bf798a6d1b45489
Gulshan011 <gulshandeep0014@gmail.com>
2023-06-20 20:41:14 +0530

    auth

--- frontend/src/context/auth.js
+++ frontend/src/context/auth.js
@@ -1,43 +1,84 @@
-import { useState,useEffect,useContext,createContext } from "react";
-import axios from "axios";
-const AuthContext = createContext()
-const AuthProvider =({children}) =>{
-    const [auth,setAuth]=useState({
-        user:null,
-        token:" "
-    })
+// import { useState,useEffect,useContext,createContext } from "react";
+// import axios from "axios";
+// const AuthContext = createContext()
+// const AuthProvider =({children}) =>{
+//     const [auth,setAuth]=useState({
+//         user:null,
+//         token:" "
+//     })
  
-    //default axios
-    axios.defaults.headers.common["Authorization"] = auth?.token;
-    useEffect(() =>{
-        const data = localStorage.getItem("auth");
-        if(data){
-            const parseData =JSON.parse(data);
-            setAuth({
-                ...auth,
-                user:parseData.user,
-                token:parseData.token,
-                bio:parseData.bio,
-            });
+//     //default axios
+//     axios.defaults.headers.common["Authorization"] = auth?.token;
+//     useEffect(() =>{
+//         const data = localStorage.getItem("auth");
+//         if(data){
+//             const parseData =JSON.parse(data);
+//             setAuth({
+//                 ...auth,
+//                 user:parseData.user,
+//                 token:parseData.token,
+//                 bio:parseData.bio,
+//             });
             
-        }
-    },[]);
-    console.log(setAuth);
-    return (
-        <AuthContext.Provider value ={{auth,setAuth}}>
-        {children}
-        </AuthContext.Provider>
-    )
-}
-//custom hooks
+//         }
+//     },[]);
+//     console.log(setAuth);
+//     return (
+//         <AuthContext.Provider value ={{auth,setAuth}}>
+//         {children}
+//         </AuthContext.Provider>
+//     )
+// }
+// //custom hooks
+
+// const useAuth = () => {
+//     const [auth, setAuth] = useContext(AuthContext);
+//     const fname = auth.user?.fname; // access fname property
+//     const bio = auth.user?.bio; 
+//     return { auth, setAuth, fname,bio}; // return auth, setAuth, and fname
+//   };
+  
+//   export { useContext,AuthContext,useAuth, AuthProvider };
+
+
+import { useState, useEffect, useContext, createContext } from "react";
+import axios from "axios";
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState({
+    user: null,
+    token: "",
+   
+  });
+
+
+
+ useEffect(() => {
+    axios.defaults.headers.common["Authorization"] = auth.token;
+  }, [auth,auth.token]);
+
+  useEffect(() => {
+    const data = localStorage.getItem("auth");
+    if (data) {
+      const parseData = JSON.parse(data);
+      setAuth(parseData);
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ auth, setAuth }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
 const useAuth = () => {
-    const [auth, setAuth] = useContext(AuthContext);
-    const fname = auth.user?.fname; // access fname property
-    const bio = auth.user?.bio; 
-    return { auth, setAuth, fname,bio}; // return auth, setAuth, and fname
-  };
-  
-  export { useContext,AuthContext,useAuth, AuthProvider };
+  const { auth, setAuth } = useContext(AuthContext);
+  const fname = auth?.user?.fname; // access fname property
+  const bio = auth?.user?.bio;
+  return { auth, setAuth, fname, bio }; // return auth, setAuth, and fname
+};
 
-
+export { useContext, AuthContext, useAuth, AuthProvider };
